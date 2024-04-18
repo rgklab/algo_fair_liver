99f7f207f002a50abaf897a5141ce14c54f97db7
Asic Chen <a19c97@hotmail.com>
2024-04-18 16:45:36 -0400

    toy example mip

--- allocation.py
+++ allocation.py
@@ -2,23 +2,37 @@ from gurobipy import *
 
 # List of organs
 omega = 3
+organs = [o for o in range(1, omega + 1)]
 
 # Organ arrival times
-organ_times = [1, 2, 3]
+organ_times = {
+    1: 1,
+    2: 2,
+    3: 3
+}
 
 # List of patients
 n = 3
+patients = [p for p in range(1, n + 1)]
 
 # Patient arrival times
-E_times = [1, 1, 1]
+E_times = {
+    1: 1,
+    2: 1,
+    3: 1
+}
 
 # Patient death times
-M_times = [2, 2, 3]
+M_times = {
+    1: 2,
+    2: 2,
+    3: 3
+}
 
 # Compute value matrix based on arrival and death times
 dict = {}
-for o in range(omega):
-    for p in range(n):
+for o, _ in enumerate(organs):
+    for p, _ in enumerate(patients):
         assignment_time = organ_times[o]
         if E_times[p] <= assignment_time <= M_times[p]:
             dict{(o, p)} = 1
@@ -35,7 +49,13 @@ x = m.addVars(assignment, name='assign')
 
 # Organ constraints
 # Each organ is assigned to exactly one patient
-organ_cstr = m.addConstrs((x.sum() == 1 for j in range(n)), name='organ')
+organ_cstr = m.addConstrs((x.sum(o, '*') == 1 for o in organs), name='organ')
 
 # Patient constraints
+# Each patient is assigned at most one organ
+patient_cstr = m.addConstrs((x.sum('*', p) <= 1 for p in patients), name='patient')
 
+# Objective function
+m.setObjective(x.prod(value), GRB.MAXIMIZE)
+
+m.optimize()
